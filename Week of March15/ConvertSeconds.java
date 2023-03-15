ec8467aff7c8cef24bb671da384aeaf4cb1d2320
OlorinGray007 <luim@alumni.upenn.edu>
2023-03-15 09:16:48 -0700

    minor syntactic changes

--- Week of March15/ConvertSeconds.java
+++ Week of March15/ConvertSeconds.java
@@ -1,13 +1,14 @@
 public class ConvertSeconds {
 
-    public static int convertSeconds(int min) {
+    public static int convert(int min) {
 
         return min*60;
 
     }
 
     public static void main(String[] args) {
-        System.out.println(convertSeconds(3));
-        System.out.println(convertSeconds(2));
+        System.out.println(convert(5));
+        System.out.println(convert(3));
+        System.out.println(convert(2));
     }
 }
